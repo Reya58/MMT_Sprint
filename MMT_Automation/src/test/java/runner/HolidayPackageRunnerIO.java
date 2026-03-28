2651649d90f097a3dfd1f5e8321222c7fa32b1b2
Biswayan Nath <biswayan6apr@gmail.com>
2026-03-28 15:18:38 +0530

    Edited Report location

--- MMT_Automation/src/test/java/runner/HolidayPackageRunnerIO.java
+++ MMT_Automation/src/test/java/runner/HolidayPackageRunnerIO.java
@@ -10,7 +10,7 @@ import utils.DriverFactory;
 @CucumberOptions(
     features = "src/test/resources/features/HolidayPackage.feature",
     glue = {"stepDefinations"},
-    plugin = {"pretty", "html:target/cucumber-reports.html",
+    plugin = {"pretty", "html:reports/HolidayPackageReports/cucumber-reports.html",
             "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
     monochrome = true
 )
